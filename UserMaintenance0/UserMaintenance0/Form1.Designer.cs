d138a69b5126a7bfc38a8ca9d70aeac41e9513f7
Ádám Kiss <adam.kiss3@stud.uni-corvinus.hu>
2021-09-29 19:27:10 +0200

    1

--- UserMaintenance0/UserMaintenance0/Form1.Designer.cs
+++ UserMaintenance0/UserMaintenance0/Form1.Designer.cs
@@ -29,37 +29,97 @@ namespace UserMaintenance0
         /// </summary>
         private void InitializeComponent()
         {
-  
-            // 
-            // txtLastName
-            // 
-           
-            // lblLastName
-            // 
-           
+            this.lblFirstName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.TextBox();
+            this.listUsers = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // lblFirstName
             // 
+            this.lblFirstName.Location = new System.Drawing.Point(342, 70);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(240, 22);
+            this.lblFirstName.TabIndex = 12;
             // 
             // btnAdd
             // 
-         
+            this.btnAdd.Location = new System.Drawing.Point(293, 112);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(289, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "button1";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
-            // txtFirstName
+            // label2
             // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.Location = new System.Drawing.Point(342, 22);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(240, 22);
+            this.lblLastName.TabIndex = 8;
+            // 
+            // listUsers
+            // 
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.ItemHeight = 16;
+            this.listUsers.Location = new System.Drawing.Point(12, 12);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(259, 420);
+            this.listUsers.TabIndex = 7;
             // 
             // button1
             // 
+            this.button1.Location = new System.Drawing.Point(293, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
+            this.button2.Location = new System.Drawing.Point(464, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-           
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.listUsers);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,6 +129,14 @@ namespace UserMaintenance0
 
         #endregion
 
+        private System.Windows.Forms.TextBox lblFirstName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lblLastName;
+        private System.Windows.Forms.ListBox listUsers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
