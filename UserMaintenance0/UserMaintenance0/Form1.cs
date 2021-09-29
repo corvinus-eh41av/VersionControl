5048c4a1669a190eec9b9b2f0604cc46d883e69e
Ádám Kiss <adam.kiss3@stud.uni-corvinus.hu>
2021-09-29 19:48:55 +0200

    mukodj

--- UserMaintenance0/UserMaintenance0/Form1.cs
+++ UserMaintenance0/UserMaintenance0/Form1.cs
@@ -18,8 +18,8 @@ namespace UserMaintenance0
         public Form1()
         {
             InitializeComponent();
-            lblLastName.Text = Resource1.LastName; 
-            lblFirstName.Text = Resource1.FirstName; 
+            lblLastName.Text = Resource1.FullName; 
+            lblFirstName.Text = Resource1.FullName; 
             btnAdd.Text = Resource1.Add;
 
             listUsers.DataSource = users;
@@ -31,8 +31,7 @@ namespace UserMaintenance0
         {
             var u = new User()
             {
-                LastName = lblLastName.Text,
-                FirstName = lblFirstName.Text
+                FullName = lblLastName.Text + " " + lblFirstName.Text
             };
             users.Add(u);
         }
