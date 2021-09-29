73b9caf6f4a986f16e46fdc83563da7ca2c8df85
Ádám Kiss <adam.kiss3@stud.uni-corvinus.hu>
2021-09-29 19:54:56 +0200

    tre

--- UserMaintenance0/UserMaintenance0/Form1.Designer.cs
+++ UserMaintenance0/UserMaintenance0/Form1.Designer.cs
@@ -96,8 +96,9 @@ namespace UserMaintenance0
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 23);
             this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
+            this.button1.Text = "Write";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
