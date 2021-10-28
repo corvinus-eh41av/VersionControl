ce6d420f901a5a6ee04a8561f1465c6411eafad6
Ádám Kiss <adam.kiss3@stud.uni-corvinus.hu>
2021-10-28 15:59:04 +0200

    haladunk

---
+++ UserMaintenance0/HW_Six/RateData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Six
+{
+    class RateData
+    {
+        DateTime Date { get; set; }
+        string Currancy { get; set; }
+        decimal Value { get; set; }
+
+    }
+}
