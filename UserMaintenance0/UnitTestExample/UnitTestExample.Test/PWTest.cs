6f76936257b1b4ef9e91a70598a100ad7d0c040e
Ádám Kiss <adam.kiss3@stud.uni-corvinus.hu>
2021-11-26 21:42:10 +0100

    commit2

---
+++ UserMaintenance0/UnitTestExample/UnitTestExample.Test/PWTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class PWTest
+    {
+        [Test,
+            TestCase("abcd1234", false),
+    TestCase("irf@uni-corvinus", false),
+    TestCase("irf.uni-corvinus.hu", false),
+    TestCase("irf@uni-corvinus.hu", true)
+            ]
+        public void TestValidatePW(string pw, bool expectedResult)
+        {
+
+            var accountController = new AccountController();
+
+
+            var actualResult = accountController.ValidatePassword(pw);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
